c1a48e9cea024e6fd2bd75512603491bcbc628c8
Sageer Shahzad <rudph1@gmail.com>
2019-04-19 11:54:21 +0100

    background animation added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return {
+    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
+  };
+}
+
+class App extends React.Component {
+  state = { styleOne: {}, styleTwo: {} };
+
+  onMouseMove = event => {
+    this.setState({
+      styleOne: transform(-event.clientX / event.clientY),
+      styleTwo: transform(event.clientX / event.clientY)
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={this.state.styleOne} />
+        <div className="panel" style={this.state.styleTwo} />
       </div>
     );
   }
